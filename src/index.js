20255968b873124d0816128c154a19c833258702
krishnaxv <krishnaxv@gmail.com>
2020-10-10 22:46:51 +0530

    fix: import required exports

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import * as Yup from 'yup'
+import { string } from 'yup'
 
-export const firstNameField = Yup.string()
+export const firstNameField = string()
   .required('First name is required.')
   .min(2, 'First name is too Short. Enter minimum 2 characters.')
 
-export const lastNameField = Yup.string()
+export const lastNameField = string()
   .required('Last name is required.')
   .min(2, 'Last name is too Short. Enter minimum 2 characters.')
 
-export const emailField = Yup.string()
+export const emailField = string()
   .required('Email address is required.')
   .email('Enter valid email address.')
 
-export const passwordField = Yup.string()
+export const passwordField = string()
   .min(4, 'Password is too Short. Enter minimum 4 characters.')
   .max(32, 'Password is too long. Enter maximum 32 characters.')
   .required('Password is required.')
 
-export const confirmPasswordField = Yup.string()
+export const confirmPasswordField = string()
   .min(4, 'Password is too Short. Enter minimum 4 characters.')
   .max(32, 'Password is too long. Enter maximum 32 characters.')
   .required('Password confirmation is required.')
-  .oneOf([Yup.ref('password')], 'Passwords must match.')
+  .oneOf([ref('password')], 'Passwords must match.')
